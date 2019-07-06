b5d2af9356553510672b36ece4e88e95bc2b8cff
robertcook <bob.cook.1966@gmail.com>
2019-07-06 16:02:24 +0100

    improve testing of ps

--- src/ps.t.ts
+++ src/ps.t.ts
@@ -6,8 +6,15 @@ import render = require('./render');
 describe('ps', function(): void {
     describe('render process list', function(): void {
         it('it calls renderTable', function() {
-            const fakeRenderTable = sinon.fake();
-            sinon.replace(render, 'renderTable', fakeRenderTable);
+            const fakeRenderTable =
+                // exercise each column accessor callback to improve coverage
+                sinon.stub(render, 'renderTable').callsFake((table: render.Table): void => {
+                    table.columns.forEach(column => {
+                        column.func({ pid: 4, command: 'test', arguments: [] });
+                    });
+                });
+            // sinon.replace(render, 'renderTable', fakeRenderTable);
+
             renderProcessList();
             assert(fakeRenderTable.calledOnce);
             sinon.restore();
